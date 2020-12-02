ff397ccbc06306d04e18fe9411683cd151dbd717
Carter J. Bastian <carter.bastian1@gmail.com>
2020-12-01 17:31:54 -0800

    work out some helper stuff

--- lib/helpers.py
+++ lib/helpers.py
@@ -4,5 +4,22 @@ def log(string):
     global DEBUG_MODE
     if DEBUG_MODE:
         print(string)
-    else:
-        print("no global debug var")
+
+def get_input(number):
+    in_str = ''
+    dir = "test" if TEST_MODE else "inputs"
+    fname = f"../{dir}/{number}.txt"
+    with open(fname, 'r') as f:
+        in_str = f.read()
+
+    log(f"Read Input from {fname}:")
+    log(in_str)
+    return in_str.string('\n')
+
+# TODO(carter): Fix this
+def get_input_by_lines(number):
+    in_str = get_input(number)
+    return (
+        [instr for instr in line.split(',')]
+        for line in in_str.strip('\n').split('\n') if line
+    )
